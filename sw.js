360f9e57e835495bf24b839eb35685f716c1fe59
tius75 <76941855+tius75@users.noreply.github.com>
2025-07-11 18:43:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,37 @@
+const CACHE_NAME = 'Pembuat-Afirmasi-Video-cache-v1';
+// Daftar file penting yang akan disimpan untuk mode offline
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './logo.png',
+  './logo192.png',
+  './logo.png'
+];
 
+// Event saat Service Worker di-install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache dibuka');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// Event saat ada permintaan jaringan (fetch)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Jika ada di cache, langsung berikan dari cache
+        if (response) {
+          return response;
+        }
+        // Jika tidak, coba ambil dari jaringan
+        return fetch(event.request);
+      }
+    )
+  );
+});
 
